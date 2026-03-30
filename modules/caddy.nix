c1d4efc321c37e248bd7792ba5de982f02938f17
James Barret <jamesbarret715@gmail.com>
2026-03-30 15:28:20 +0100

    caddy changes

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -4,8 +4,8 @@
 	services.caddy = {
 		enable = true;
 		package = pkgs.caddy.withPlugins {
-			plugins = [ "github.com/caddy-dns/cloudflare@0.2.4"];
-
+			plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4"];
+			hash = "sha256-8HpPZ/VoiV/k0ZYcnXHmkwuEYKNpURKTN19aYZRLPoM=";
 		};
 
 		environmentFile = config.age.secrets.cloudflare.path;
