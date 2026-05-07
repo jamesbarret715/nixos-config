f5b628510b1e08a3040c35572e4975ad01edb7c0
James Barret <jamesbarret715@gmail.com>
2026-05-07 11:19:53 +0100

    fix(gaming): disable gpu compositing for steam and cleanup gamemode

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -1,10 +1,14 @@
-{ pkgs, ... }: {
-	flake.modules.nixos.gaming = { ... }: {
+{
+	flake.modules.nixos.gaming = { pkgs, ... }: {
 		programs.steam = {
 			enable = true;
+			package = pkgs.steam.override {
+				extraArgs = "-cef-disable-gpu-compositing";
+			};
+
 			remotePlay.openFirewall = true;
 			dedicatedServer.openFirewall = true;
-			gamescopeSession.enable = true;
+			# gamescopeSession.enable = true;
 		};
 
 		programs.gamemode = {
@@ -14,10 +18,6 @@
 					# Use P-cores (0-11) for gaming on 12th/13th gen Intel
 					# This runs when a game starts
 					start = "${pkgs.util-linux}/bin/taskset -pc 0-11 $GAMEMODE_G_PID";
-				};
-				custom = {
-					start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-					end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
 				};
 			};
 		};
@@ -31,10 +31,5 @@
 			mangohud
 			protonup-qt
 		];
-
-		# Force dGPU for Steam via environment if nvidia-offload is available
-		# Note: Users can also use 'nvidia-offload steam' or set launch options in steam
-		# setting __NV_PRIME_RENDER_OFFLOAD=1 etc globally might be too much, 
-		# so we just ensure the tools are there.
 	};
 }
